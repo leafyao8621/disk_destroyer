7d4de369803beaf4ff3eaa6b9c3a332a0196e63e
Yi Yao <leaf_yao8621@hotmail.com>
2024-12-31 15:32:38 -0800

    Updates

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -3,18 +3,68 @@
 #include "config/config.h"
 #include "writer/writer.h"
 
+const static char *msg =
+    "Usage: dide [OPTIONS] <DEVICE>\n"
+    "Arguments:\n"
+    "<DEVICE>\n"
+    "Options:\n"
+    "-b <BLOCK SIZE>\n"
+    "-p <PATTERN>\n"
+    "    Acceptable values:\n"
+    "        itsg, ict, hmg_baseline, hmg_enhanced, guttman\n"
+    "-v";
+
 int main(int argc, char **argv) {
     if (!argc) {
-        std::cout << "USAGE" << std::endl;
+        std::cout << msg << std::endl;
         return 0;
     }
     size_t buf_size = 4096;
     bool verbose = false;
-    verbose = true;
+    std::string pattern = "itsg";
+    int ret = 0;
+    for (; (ret = getopt(argc, argv, "b:p:v")) != -1;) {
+        switch (ret) {
+        case 'b':
+            buf_size = atol(optarg);
+            break;
+        case 'p':
+            pattern = optarg;
+            break;
+        case 'v':
+            verbose = true;
+            break;
+        case ':':
+            std::cerr << msg << std::endl;
+            return -1;
+        case '?':
+            std::cerr << msg << std::endl;
+            return -1;
+        }
+    }
+    if (optind != argc - 1) {
+        std::cerr << msg << std::endl;
+        return -1;
+    }
+    char *pattern_compiled = nullptr;
+    if (pattern == "itsg") {
+        pattern_compiled = (char*)DiskDestroyer::Config::itsg;
+    } else if (pattern == "ict") {
+        pattern_compiled = (char*)DiskDestroyer::Config::ict;
+    } else if (pattern == "hmg_baseline") {
+        pattern_compiled = (char*)DiskDestroyer::Config::hmg_baseline;
+    } else if (pattern == "hmg_enhanced") {
+        pattern_compiled = (char*)DiskDestroyer::Config::hmg_enhanced;
+    } else if (pattern == "guttman") {
+        pattern_compiled = (char*)DiskDestroyer::Config::guttman;
+    } else {
+        std::cerr << msg << std::endl;
+        return -1;
+    }
     try {
-        DiskDestroyer::Writer writer(argv[1], buf_size, verbose);
+        DiskDestroyer::Writer writer(argv[optind], buf_size, verbose);
         writer.init();
-        writer((char*)DiskDestroyer::Config::guttman);
+        writer(pattern_compiled);
     } catch (DiskDestroyer::Gen::Err err) {
         switch (err) {
         case DiskDestroyer::Gen::Err::OPEN:
